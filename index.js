ec3aae2edf33e7e6cb1c3e253cba34948fb820a1
Abhay <abakshi4@my.bcit.ca>
2022-03-03 13:31:57 -0800

    connect to databse

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const config = require ('./config')
+
+
+app.use(express.json()) 
+mongoose.connect(config.Mongo_DB_URL, (err)=>{
+    if(err) return console.log(err)
+
+    console.log("connected to db successfully")
+})
+
+app.listen(3000, ()=> console.log('server running on port 3000'))
