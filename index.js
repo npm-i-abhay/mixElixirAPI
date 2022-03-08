612a64e33c1f4e9558450e2c79ef611d02121428
Abhay <abakshi4@my.bcit.ca>
2022-03-08 15:31:03 -0800

    fixing errors

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ mongoose.connect(config.Mongo_DB_URL, (err)=>{
 
 app.listen(3000, ()=> console.log('server running on port 3000'))
 
-const res = await axios.get('');
-console.log(res)
-const config = require ('./config')
+// const res = await axios.get('');
+// console.log(res)
+// const config = require ('./config')
