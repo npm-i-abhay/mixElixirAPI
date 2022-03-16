c4f21ef39f48ef6e3fe669f062b1fab14c7f58f4
Abhay <abakshi4@my.bcit.ca>
2022-03-16 16:55:25 -0700

    updating user schema

--- utils/controller/user.js
+++ utils/controller/user.js
@@ -4,6 +4,7 @@ const signup = (req,res)=>{
     const user = new User()
     user.email = req.body.email
     user.password = req.body.password
+    user.username = req.body.username
     user.save((err,done)=>{
         if(err) return res.status(500).send('sign up failed')
         res.status(201).send("Signed up successfully")
@@ -15,12 +16,13 @@ const login = (req,res)=>{
 User.findOne({email:req.body.email}, (err,user)=>
     {
         if(err || !user ) return res.status(500).send('user not found')
+        console.log(req.body)
         if(user.comparePassword(req.body.password))
         {   const token = jwt.sign({ id:user._id }, 'mysecret')
-            res.send(token)
+            res.status(201).send(token)
         }
         else {
-            res.send('could not login')
+            res.status(500).send('could not login')
         }
     })
 }
--- utils/model/user.js
+++ utils/model/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcryptjs')
 const UserSchema = new Schema({
-
+    username:String,
     email:String,
     password: String,
     // defining the user Schema
