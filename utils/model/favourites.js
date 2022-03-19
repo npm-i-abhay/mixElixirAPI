0ff032b7c1761d0627b80c333385e0d0bcdd2f1a
Abhay <abakshi4@my.bcit.ca>
2022-03-18 17:06:49 -0700

    updating the user model to [] instead of [{}]

--- utils/model/favourites.js
+++ utils/model/favourites.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const FavouriteScehma = new Schema({
-        drinks : [{}],
+        drinks : [],
 })
 
 const Favourites = mongoose.model("Favs", FavouriteScehma)
