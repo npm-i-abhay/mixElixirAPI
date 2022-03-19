46e0d17fe0f43bf154ec65e1c35ebc4c0ead2ee9
Abhay <abakshi4@my.bcit.ca>
2022-03-18 20:04:00 -0700

    fixing error for favDrink

--- utils/controller/favourites.js
+++ utils/controller/favourites.js
@@ -4,7 +4,7 @@ const Favourites = require ('../model/favourites')
 const addFavourite = (req,res)=>
 {
     var favs = new Favourites()
-    favs.drink = req.body.favDrinks
+    favs.drink = req.body.favDrink
     favs.user = req.body.user
     favs.save((err,done)=>{
         if(err) return res.status(500).send('can create any favourites')
